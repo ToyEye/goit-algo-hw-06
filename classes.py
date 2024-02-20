bd03839760c148ca3bc174a470a4f0f6753bb266
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-02-20 21:59:12 +0200

    fin

--- classes.py
+++ classes.py
@@ -8,7 +8,7 @@ class Field:
         return str(self.value)
 
 class Name(Field):
-    # реалізація класу
+    
     def __init__(self, value):
         if len(value) !=0:
             super().__init__(value)
@@ -17,7 +17,7 @@ class Name(Field):
 		
 
 class Phone(Field):
-    # реалізація класу
+    
     def __init__(self, value):
         if len(value) == 10:
             super().__init__(value)
@@ -30,25 +30,23 @@ class Record:
         self.name = Name(name)
         self.phones = []
 
-    # реалізація класу
-
     def add_phone(self,phone):
-        self.phones.append(Phone(phone))
+        self.phones.append(Phone(phone)) # додавання  телефона за допомогою класа Phone
         
     def remove_phone(self, phone):
         for el in self.phones:
             if el.value == phone:
-                self.phones.remove(el)
+                self.phones.remove(el) #видалення телефону
         
     def edit_phone(self,old_phone,new_phone):
         for phone in self.phones:
             if phone.value == old_phone:
-                phone.value = new_phone
+                phone.value = new_phone   #зміна номера
     
     def find_phone(self,phone):
         for el in self.phones:
             if el.value == phone:
-                return el.value
+                return el.value         #пошук номера
     
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
@@ -56,26 +54,24 @@ class Record:
 
 class AddressBook(UserDict):
     def add_record(self,record):
-        self.data[record.name.value]=record
+        self.data[record.name.value]=record  #додавання запису
 
     def find(self, name):
-
-        return self.data.get(name)
-
+        return self.data.get(name)   #пошук запису
             
     def delete(self, name):
-        if name.lower() in self.data:
-            del self.data[name.lower()]
+        if name in self.data:
+           del self.data[name]     #видалення запису
             
 # Створення нової адресної книги
 book = AddressBook()
 
-    # Створення запису для John
+# Створення запису для John
 john_record = Record("John")
 john_record.add_phone("1234567890")
 john_record.add_phone("5555555555")
 
-    # Додавання запису John до адресної книги
+# Додавання запису John до адресної книги
 book.add_record(john_record)
 
 # Створення та додавання нового запису для Jane
@@ -83,22 +79,19 @@ jane_record = Record("Jane")
 jane_record.add_phone("9876543210")
 book.add_record(jane_record)
 
-# Виведення всіх записів у книзі
-
+ # Виведення всіх записів у книзі
 for name, record in book.data.items():
     print(record)
-    
+
 # Знаходження та редагування телефону для John
 john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
 
-# john.edit_phone("1234567890", "1112223333")
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
 
-# print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
-# Пошук конкретного телефону у записі John
-# found_phone = john.find_phone("5555555555")
-# print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+ # Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
 # Видалення запису Jane
 book.delete("Jane")
-
-for name, record in book.data.items():
-    print(record)
